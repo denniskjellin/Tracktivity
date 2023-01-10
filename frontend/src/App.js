1128324e0a41b3fd2e42ff1c9f58d07fcb4083ab
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-10 17:36:57 +0100

    Installed react-bootstrap, and seting up files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import "./App.scss";
+import './index.css';
 
 function App() {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
