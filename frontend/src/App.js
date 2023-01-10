1732094404efaa4302a12bfa4e9de5aa793ee0bc
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-10 18:29:36 +0100

    Added a Navbar to my page and a Custom file to create custom colors for SASS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.css';
 import "./App.scss";
 import './index.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Hello World</h1>
     </div>
   );
