9bbffba88c9f61f9af735f75c8b65c45c298eba6
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-13 00:14:06 +0100

    nulled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.scss";
 import "./index.css";
 import MainLayout from "./layout/MainLayout";
-import AddSession from "./components/AddSession";
+
 
 function App() {
   return (
     <MainLayout>
       <h1>Hello from App</h1>
-      <AddSession />
+
     </MainLayout>
   );
 }
--- frontend/src/pages/SummaryPage.jsx
+++ frontend/src/pages/SummaryPage.jsx
@@ -1,34 +1,12 @@
 import React from "react";
 import MainLayout from "../layout/MainLayout";
-import axios from "axios"
-import {useState, useEffect} from "react";
 
 function SummaryPage() {
-  const baseUrl = "http://localhost:3000/sessions"
-const [sessions, setSession] = useState([])
 
-useEffect(() =>{
-  fetchData();
-}, [])
 
-const fetchData = async () => {
-  const {data} = await axios.get(baseUrl)
-
-  setSession(data)
-}
-  
   return (
     <MainLayout>
-      <h1>Hello from Mainlayout</h1>
-      {sessions.map(post => (
-        <div key={post.id}>
-          <h5>{post.activity}</h5>
-          <p>Delete</p>
-          <p>{post.duration}</p>
-          <p>{post.comment}</p>
-          <p>{post.date}</p>
-        </div>
-      ))}
+    
     
     </MainLayout>
   );
