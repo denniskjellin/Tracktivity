ced296904450a922d5fa71d6201178f673a46816
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-13 13:49:24 +0100

    name change on GetSessions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.scss";
 import "./index.css";
 import MainLayout from "./layout/MainLayout";
-import GetSessions from "./components/SessionComponent";
+
 
 function App() {
   return (
     <MainLayout>
       <h1>Hello from App!</h1>
-      <GetSessions></GetSessions>
+    
     </MainLayout>
   );
 }
--- frontend/src/pages/SummaryPage.jsx
+++ frontend/src/pages/SummaryPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import MainLayout from "../layout/MainLayout";
-
+import GetSessions from "../components/GetSessions";
 function SummaryPage() {
 
 
   return (
     <MainLayout>
     
-    
+    <GetSessions></GetSessions>
     </MainLayout>
   );
 }
