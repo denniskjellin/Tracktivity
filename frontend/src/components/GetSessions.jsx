301bc24bb62ed967fd26db3d0abf2ac93dddfc69
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-13 21:33:32 +0100

    css changes

--- frontend/src/components/GetSessions.jsx
+++ frontend/src/components/GetSessions.jsx
@@ -1,26 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import myImage from '../images/dumbell.png'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import myImage from "../images/dumbell.png";
 
 function GetSessions() {
-
-  
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const url = 'http://localhost:3000/sessions'
+  const url = "http://localhost:3000/sessions";
   useEffect(() => {
-    axios.get(url)
-      .then(response => {
+    axios
+      .get(url)
+      .then((response) => {
         setData(response.data);
-        console.log(response.data)
+        console.log(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         setError(error);
       });
   }, []);
 
-  const deleteEndpoint = 'http://localhost:3000/sessions';
+  const deleteEndpoint = "http://localhost:3000/sessions";
   const clickDelete = async (id) => {
     try {
       // add id to the deleteEndpoint
@@ -31,28 +29,39 @@ function GetSessions() {
     } catch (error) {
       console.log(error);
     }
-  }
-
+  };
 
   if (error) {
     return <p>An error occurred: {error.message}</p>;
   } else if (data) {
     return (
-      <section style={{
-        display: 'flex', flexFlow: 'row', flexWrap: 'wrap'}
-    }
-    >
-        {data.map(data => (
-          
+      <section
+        style={{
+          display: "flex",
+          flexFlow: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        {data.map((data) => (
           <div key={data._id} className="col-sm-12 col-md-5 m-1 card">
-            <img className="dumbell card-img-top" src={myImage} alt="dumbell logo" />
+            <img
+              className="dumbell card-img-top"
+              src={myImage}
+              alt="dumbell logo"
+            />
             <div className="card-body">
-            <h2 className="h4">Exercise - {data.exercise}</h2>
-            <h3 className="h5">Reps: </h3><p>{data.reps}</p>
-            <h3 className="h5">Weight: </h3><p>{data.weight}</p>
-            <h3 className="h5">Date: </h3><p>{data.date}</p>
-            <button className="delete" onClick={() => clickDelete(data._id)}>Delete</button><br></br>
-            <br></br>
+              <h2 className="h4">Exercise - {data.exercise}</h2>
+              <h3 className="h5">Reps: </h3>
+              <p>{data.reps}</p>
+              <h3 className="h5">Weight: </h3>
+              <p>{data.weight}</p>
+              <h3 className="h5">Date: </h3>
+              <p>{data.date}</p>
+              <button className="delete" onClick={() => clickDelete(data._id)}>
+                Delete
+              </button>
+              <br></br>
+              <br></br>
             </div>
           </div>
         ))}
